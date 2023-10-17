fae5c3ca882586b583c95ee448c05d426c04b033
fish-and-bear <jessegarfieldscats@gmail.com>
2023-10-17 15:35:27 +0800

    FEAT: Generate both individual and combined histograms

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug 27 20:39:51 2023
-
 @author: 王安吉
+
 """
 
 import os
@@ -10,44 +9,55 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import cv2
 
+# Define the folder path
 folder_path = 'Pics'  # Update folder_path to your image folder
 
 # Empty list to store histograms
 histograms = []
 
-i = 0
-for filename in os.listdir(folder_path):  
+# Iterate through the files in the folder
+for filename in os.listdir(folder_path):
     if filename.endswith('.jpg') or filename.endswith('.png'):
         image_path = os.path.join(folder_path, filename)
         image = Image.open(image_path)
-
+        
         plt.figure()
         plt.imshow(image)
         plt.show()
         image.close()
         
-        i += 1
         img = cv2.imread(image_path)
-        # cv2.imshow("Img "+str(i),img)
-        cv2.waitKey()
         
-        # histogram
-        color = ('b','g','r')
+        # Histogram for each color channel
+        colors = ['b', 'g', 'r']
+        num_bins = 256  # Number of bins for the histograms
+        
+        # Calculate and store histograms
+        for idx, channel_color in enumerate(colors):
+            histogram = cv2.calcHist([img], [idx], None, [num_bins], [0, num_bins])
+            
+            plt.style.use('dark_background')
+            plt.figure(figsize=(10, 5))
+            plt.plot(histogram, color=channel_color)
+            plt.xlim([0, num_bins])
+        
+        # Split the image into color channels (BGR format)
+        blue, green, red = cv2.split(img)
+        
         plt.style.use('dark_background')
-        plt.figure(figsize=(10,5))
-        for idx, color in enumerate(color):
-            histogram = cv2.calcHist([img],[idx],None,[256],[0, 256])
-            plt.plot(histogram, color = color)
-            plt.xlim([0, 256])
+        plt.figure(figsize=(10, 5))
+        
+        for i, color in enumerate(colors):
+            histogram = cv2.calcHist([img], [i], None, [num_bins], [0, num_bins])
+            plt.plot(histogram, color=color)
             histograms.append(histogram)  # Append the histogram to the list
             
         plt.show()
-cv2.destroyAllWindows()
 
 # Plot all histograms on a single graph
 plt.style.use('dark_background')
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(10, 5))
 for histogram in histograms:
     plt.plot(histogram)
-    plt.xlim([0, 256])
+    plt.xlim([0, num_bins])
 plt.show()
